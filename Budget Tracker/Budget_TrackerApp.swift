c8ec8fa6a8b820718e5895da0cd5cadfa2e0d2d6
Oncu <oohancan@gmail.com>
2024-12-01 21:58:20 +0300

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Oncu Can on 1.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
